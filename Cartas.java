f87f04cb3ec0a8b3f988c1eebe1ab6c8981a8655
edu82 <eduardo@philipsoft.cl>
2023-02-17 15:33:53 -0300

    mayorNoDuplicado.

--- Cartas.java
+++ Cartas.java
@@ -4,7 +4,7 @@ public class Cartas {
     public static void main(String[] args) {
         List<int[]> jugadas = jugadas(5);
 
-        System.out.println("List.toArray: " + Arrays.deepToString(jugadas.toArray())); //imprime el list.toArray
+        System.out.println("List.toArray: " + Arrays.deepToString(jugadas.toArray())); // imprime el list.toArray
 
         /*
          * Genera un arreglo multidimensional del tamaño de la lista
@@ -14,14 +14,17 @@ public class Cartas {
         for (int i = 0; i < jugadas.size(); i++) {
             a[i] = jugadas.get(i);
         }
-        System.out.println("ARRAY: " + Arrays.deepToString(a)); //imprime el arreglo multidimensional
+        System.out.println("ARRAY: " + Arrays.deepToString(a)); // imprime el arreglo multidimensional
 
-        int[] arre = aplanar(a); //aplana el arreglo multidimensional
-        System.out.println("arreglo aplanado: " + Arrays.toString(arre)); //imprime el arreglo aplanado (unidimensional)
+        int[] arre = aplanar(a); // aplana el arreglo multidimensional
+        System.out.println("arreglo aplanado: " + Arrays.toString(arre)); // imprime el arreglo aplanado
+                                                                          // (unidimensional)
+        System.out.println("Carta mayor no duplicada: " + winningCard(arre));
     }
 
     /*
-     * Genera lista con N cantidad de arreglos de dimension al azar con valores al azar
+     * Genera lista con N cantidad de arreglos de dimension al azar con valores al
+     * azar
      * donde N es la cantidad de jugadores q recibe como parametro
      */
     private static List<int[]> jugadas(int jugadores) {
@@ -42,9 +45,12 @@ public class Cartas {
 
     /*
      * Toma un arreglo de int multidimencional,
-     * lo recorre para calcular el tamaño total debido a que cada arreglo contenido puede tener una dimension diferente
-     * Lo vuelve a recorrer, almacenando en un arreglo unidimensional que se creo con el tamaño calculado del multidimensional
-     * lo llena con los valores del multidimensional y devuelve el arreglo unidimensional.
+     * lo recorre para calcular el tamaño total debido a que cada arreglo contenido
+     * puede tener una dimension diferente
+     * Lo vuelve a recorrer, almacenando en un arreglo unidimensional que se creo
+     * con el tamaño calculado del multidimensional
+     * lo llena con los valores del multidimensional y devuelve el arreglo
+     * unidimensional.
      */
     private static int[] aplanar(int[][] arreglo) {
         int acumulador = 0;
@@ -62,4 +68,20 @@ public class Cartas {
         return arregloAplanado;
     }
 
+    private static int winningCard(int[] jugadas) {
+        int mayorNoduplicado = -1;
+        Arrays.sort(jugadas);
+        System.out.println("Arreglo ordenado: " + Arrays.toString(jugadas));
+        for (int i = 0; i < jugadas.length; i++) {
+            // Verificar si el elemento actual es igual al elemento anterior o al siguiente
+            if ((i > 0 && jugadas[i] == jugadas[i - 1]) || (i < jugadas.length - 1 && jugadas[i] == jugadas[i + 1])) {
+                continue;
+            } else {
+                //al estar ordenado de menor a mayor el valor no duplicado que se agrega, será reemplazado por uno mayor no duplicado en cada iteración.
+                mayorNoduplicado = jugadas[i];
+            }
+        }
+
+        return mayorNoduplicado;
+    }
 }
